15ff5b6c2adf39a746fa78e24d0200490aab3135
jefrydep <jefrydep@gmail.com>
2022-11-27 15:31:54 -0600

    adding changes from other pc

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke_response.interface';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+    constructor(
+      @InjectModel(Pokemon.name)
+      private readonly pokemonModel:Model<Pokemon>
+    ){}
+
   async executedSeed() {
     const URL = 'https://pokeapi.co/api/v2/pokemon?limit=600';
     const { data } = await axios.get<PokeResponse>(URL);
